4114004efa502865646696351ab794648bfce9d3
Ian Graham <imartingraham@gmail.com>
2013-03-05 21:35:37 -0800

    Added the TT class to the index.js file export

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ Nomniture = {};
 Nomniture.Client = require('./lib/client');
 Nomniture.Report = require('./lib/report');
 Nomniture.Saint = require('./lib/saint');
+Nomniture.TT = require('/lib/tt');
 module.exports = Nomniture;
