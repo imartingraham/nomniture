dcd23a40f6bb2c4c9b88e17a3eaf2298afdc6b2c
Timothy Elliott <tle@holymonkey.com>
2014-01-06 14:16:48 -0800

    Tests - Use configured report suite ID.
    Also adds emailAddress to the example test credentials.

--- test/test-credentials-example.js
+++ test/test-credentials-example.js
@@ -1,5 +1,6 @@
 module.exports = {
 	username: 'username:Company Name',
 	sharedSecret: 'Long String Of Text',
-	reportSuiteId: 'reportSuiteId'
+	reportSuiteId: 'reportSuiteId',
+	emailAddress: 'me@domain.com'
 };
